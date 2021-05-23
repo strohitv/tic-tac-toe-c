a1ad2e291203e45277bb3cfc656c1e18b4bf9a6f
strohitv <the.real.strohi@gmail.com>
2021-05-23 16:37:42 +0200

    move most code files to game directory

--- main.h
+++ main.h
@@ -5,9 +5,9 @@
 #define player1Char 'O'
 #define player2Char 'X'
 
-#include "utils.h"
-#include "score.h"
-#include "player.h"
-#include "ai.h"
+#include "game/utils.h"
+#include "game/score.h"
+#include "game/player.h"
+#include "game/ai.h"
 
 #endif //INC_11_TIC_TAC_TOE_MAIN_H
